51919c814f10011d58d2e96fad13895532760caa
jiyoung <ij0512@naver.com>
2023-12-13 12:40:23 +0900

    static 공부

--- src/ex02/IdenEx01.java
+++ src/ex02/IdenEx01.java
@@ -1,2 +1,21 @@
-package ex02;public class IdenEx01 {
+package ex02;
+
+/**
+ * 변수 작성법
+ * 1. 카멜(낙타)표기법 사용
+ * 2. 숫자를 먼저 사용하지 않는다.
+ * 3. 특수문자는 사용하지 않는다.
+ */
+public class IdenEx01 {//클래스명은 파스칼표기법
+    public static void main(String[] args) {
+        //int class; 키워드로 쓰고 있어서 사용할 수 없음
+        int abc__; //절대 사용안함!!
+        int userMoney; //변수명은 카멜 표기법(낙타표기법)
+        //int abc#;
+        int method;
+        double abc$$; //특수문자 사용하지 말자
+        //int 1stMoney; //숫자로 변수를 시작할 수 없음
+
+
+    }
 }
--- src/ex04/MemEx01.java
+++ src/ex04/MemEx01.java
@@ -1,2 +1,21 @@
-package ex04;public class MemEx01 {
+package ex04;
+
+/**
+ * 아키텍처
+ * 1. JVM메모리에 static을 찾는다. 넣는다
+ * 2. .class를 불러온다
+ * 3. class 공간을 만든다
+ * 4. 공간에 static명 을 넣는다
+ * 5. main을 실행시킨다
+ * 6. main이 끝날 때까지 static은  띄워놓는다
+ */
+class Person1{//커스텀 자료형
+    static int age = 20;
+    static char gender = '여';
+}
+public class MemEx01 {
+    public void main(String[] args) {
+        System.out.println(Person1.age);
+        System.out.println(Person1.gender);
+    }
 }
